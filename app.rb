82e3d782e4b6e75d51712f82c1b8dc4e750746ca
iMprov <improv00@mail.ru>
2025-09-15 16:11:44 +0700

    Основа меню

--- app.rb
+++ app.rb
@@ -4,21 +4,25 @@ require 'win32/sound'
 require 'json'
 include Win32
 
+class GameChapter
+  
+
+  def chapter
+    
+  end
+end
+
 class PlayerStatistics 
   def initialize
     @user = UserJSON.new.parsed_data
   end
 
   def stat_player
-    puts "Статистика"
+    puts "\tСтатистика"
     puts "Имя: #{@user[:name]}"
     puts "Пройдено квестов: #{@user[:quests].length}"
     puts "Побеждено боссов: #{@user[:bosses].length}"
     puts "Уровень: #{( Math.sqrt(@user[:xp]) / 5 ).round} || XP: #{@user[:xp]}"
-  end
-
-  def user_json
-    
   end
 end
 
@@ -41,6 +45,30 @@ class UserJSON
   end
 end
 
+class GameJSON
+  attr_reader :parsed_data
+
+  def initialize
+    @file_path = File.join(__dir__, 'game_map.json') #сохраняем в переменную путь к файлу
+    read_json
+  end
+
+  def read_json #метод чтения и парсинга 
+    json_data = File.read(@file_path) # присваиваем переменной команду прочитать файл по пути
+    @parsed_data = if File.exist?(@file_path)  #парсим файл
+             json_data = File.read(@file_path)
+             JSON.parse(json_data, symbolize_names: true)
+           else
+             {} # Если файла нет, создаем пустой массив задач
+           end #парсим данные в переменную
+  end
+end
+
+class Messages
+  def error
+    puts "Некорректный ввод!"
+  end
+end
 
 module Helper
   module_function
@@ -48,7 +76,6 @@ module Helper
   def clear
     print "\e[H\e[2J"
   end
-
 end
 
 
@@ -63,7 +90,7 @@ end
 # end
 
 start = StartMessage.new
-
+message = Messages.new
 #start.new_start
 
 Helper.clear
@@ -75,10 +102,33 @@ Helper.clear
 statistics = PlayerStatistics.new
 statistics.stat_player
 sleep(5)
-Helper.clear
 
 loop do 
+  Helper.clear
+
+  puts "\tМеню"
+  puts "1. Отправиться в путь"
+  puts "2. Посмотреть статистику"
+  puts "3. Выйти"
+  print "Ввод: "
+  input = gets.to_i
+
+  case input
+
+  when 1
 
 
+
+  when 2
+    Helper.clear
+    statistics.stat_player
+    sleep(5)
+  when 3
+    exit
+  else
+    Helper.clear
+    message.error
+    sleep(2)
+  end
 
 end
