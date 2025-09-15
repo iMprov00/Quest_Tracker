1c534666acbedd298345e327e6b5002263fafdc0
iMprov <improv00@mail.ru>
2025-09-15 14:33:53 +0700

    Запуск аудио

--- app.rb
+++ app.rb
@@ -1,17 +1,33 @@
 require 'colorize'
 require_relative 'start_message'
+require 'win32/sound'
+include Win32
 
+module ClearScreen
+  module_function
 
-def clear_screen
-  print "\e[H\e[2J"
+  def clear
+    print "\e[H\e[2J"
+  end
 end
 
 
+# Запускаем воспроизведение звука в отдельном потоке
+sound_thread = Thread.new do
+  begin
+    # Попробуйте использовать ASYNC, если это возможно
+    Sound.play('sound.wav', Sound::ASYNC) # Если не работает, оставьте Sound.play('sound.wav')
+  rescue => e
+    puts "Ошибка воспроизведения звука: #{e.message}"
+  end
+end
+
 start = StartMessage.new
+
 
 start.new_start
 
-clear_screen
+ClearScreen.clear
 
 puts "Добро пожаловать в RUBY REALMS!"
 
